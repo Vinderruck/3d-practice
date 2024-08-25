97c709dcaa21dfb27c3f0e61997143bdc9d5bd39
Vinderruck <majevincent0@gmail.com>
2024-08-25 13:02:36 +0300

    change

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -34,16 +34,28 @@ return(
     className="rounded-2xl w-full h-full object-cover"
     />
     <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
-      <div onClck={()=>window.open(source_code-Link,"_blank")}
+      <div onClick={()=>window.open(source_code-Link,"_blank")}
         className="flex justify-center items-center rounded-full w-10 h-10 cursor-pointer black-gradient">
 
  <img src={github}  alt="github" className="w-1\2 h-1\2 object-contain"/> </div></div>
     </div>   
     
-    <div className="mt-5">
-      <h3 className="font-bold text-[24px] text-white">{name}</h3>
-      <p className="mt-2 text-[14px] text-secondary">{description}</p>
-    </div>
+   
+    <div className='mt-5'>
+          <h3 className='font-bold text-[24px] text-white'>{name}</h3>
+          <p className='mt-2 text-[14px] text-secondary'>{description}</p>
+        </div>
+
+        <div className='flex flex-wrap gap-2 mt-4'>
+          {tags.map((tag) => (
+            <p
+              key={`${name}-${tag.name}`}
+              className={`text-[14px] ${tag.color}`}
+            >
+              #{tag.name}
+            </p>
+          ))}
+        </div>
      </Tilt></motion.div>
 )
 }
